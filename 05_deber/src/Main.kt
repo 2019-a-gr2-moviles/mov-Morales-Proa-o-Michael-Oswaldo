f600ab83aaa45a9eb41927bc3f4af75fef964cd7
MichaelDrake <41341651+MichaelDrake@users.noreply.github.com>
2019-05-06 23:43:41 -0500

    mostrar

--- 05_deber/src/Main.kt
+++ 05_deber/src/Main.kt
@@ -1,10 +1,14 @@
 import java.io.File
 import java.util.*
 import javax.swing.JOptionPane
+import kotlin.collections.ArrayList
 
 fun main(args: Array<String>){
     println("Hola perros")
-    val user = JOptionPane.showInputDialog("Ingrese su nombre de usuario:")
+    val user = JOptionPane.showInputDialog("   Bienvenido a 'My PetShop' " +
+            "\n" +
+            "Ingrese su nombre de usuario:" +
+            "")
     val password = JOptionPane.showInputDialog("Ingrese su password:")
     if (validar(user,password)){
         if (user == "admin"){
@@ -12,7 +16,7 @@ fun main(args: Array<String>){
         }
         else{
 
-            adoptar_mascota()
+            adoptar_mascota(user)
         }
 
     }
@@ -44,9 +48,9 @@ fun validar(usuario:String,contraseña:String):Boolean{
 
 }
 
-fun adoptar_mascota(){
+fun adoptar_mascota(nombre:String){
 
-    JOptionPane.showMessageDialog(null,"Bienvenidos a 'My PetShop', click en OK para continuar ")
+    JOptionPane.showMessageDialog(null,"Bienvenido $nombre \n Presiona en OK para continuar ")
 
     JOptionPane.showInputDialog("¿Que desea hacer? \n " +
             "1. Mostrar\n" +
@@ -57,7 +61,61 @@ fun adoptar_mascota(){
 }
 
 fun crud(){
+    val path = "./src/mascotas.txt"
+do {
+    val accion = JOptionPane.showInputDialog(
+        "¿Que desea realizar? \n" +
+                "1. Mostrar Mascotas\n" +
+                "2. Crear Mascota\n" +
+                "3. Editar Mascota\n" +
+                "4. Eliminar Macota\n" +
+                "5. Salir"
+    )
+    if(accion == "1"){
+file_handler(path,"Mostrar")
+    }
+    if(accion == "2"){
 
+    }
+    if(accion == "3"){
+
+    }
+    if(accion == "4"){
+
+    }
+    if(accion == "5"){
+
+    }
+
+}while(accion != "5")
+
+}
+
+
+fun file_handler(path: String, accion:String){
+    var mostrar = "Código:      Tipo:        Nombre:\n"
+    val arreglo_usuarios = ArrayList<String>()
+    if (accion == "Mostrar" ) {
+
+       File(path).forEachLine { linea ->
+           arreglo_usuarios.add(linea)
+
+       }
+
+arreglo_usuarios.forEach {
+    mostrar = mostrar + "        "+it.split(',')[2]+"          "+it.split(',')[1]+"        "+it.split(',')[0]+"\n"
+}
+        JOptionPane.showMessageDialog(null, mostrar)
+   }
+    else if(accion == "Crear"){
+
+
+   }else if(accion == "Editar"){
+
+   }else if(accion == "Eliminar"){
+
+
+   }
 
 
 }
