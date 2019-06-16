537e569abd6ea4b59d8071bb77134e72a08c61f4
MichaelDrake <41341651+MichaelDrake@users.noreply.github.com>
2019-06-16 01:08:22 -0500

    messsemger ok

---
+++ 02_Proyecto/Deber/Mssenger/app/src/main/java/com/example/mssenger/Mensaje.kt
@@ -0,0 +1,4 @@
+package com.example.mssenger
+
+class Mensaje(var autor: String, var mensaje:String, var hora: String, var id_usuario: Int ){
+}
--- 02_Proyecto/Messenger/app/src/main/java/com/example/messenger/Mensaje.kt
+++
@@ -1,4 +0,0 @@
-package com.example.messenger
-
-class Mensaje(var autor: String, var mensaje:String, var hora: String, var id_usuario: Int ){
-}
