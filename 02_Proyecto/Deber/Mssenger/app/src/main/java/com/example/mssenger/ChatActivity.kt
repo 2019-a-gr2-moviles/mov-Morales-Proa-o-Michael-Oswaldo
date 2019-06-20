219d93039ef16d20e31eeac96610fd3177042750
MichaelDrake <41341651+MichaelDrake@users.noreply.github.com>
2019-06-20 09:43:15 -0500

    messenger azul

--- 02_Proyecto/Deber/Mssenger/app/src/main/java/com/example/mssenger/ChatActivity.kt
+++ 02_Proyecto/Deber/Mssenger/app/src/main/java/com/example/mssenger/ChatActivity.kt
@@ -23,15 +23,24 @@ class ChatActivity : AppCompatActivity() {
 
 
 
-        listachat.add(Chat("Este easdasdasdasdasd jhg hjg jhg hjg jhg ","este easdasdasdasdasdasdasdasd"))
-        listachat.add(Chat("Este easdasdasdasdasd","este easdasdasdasdasdasdasdasd"))
-        listachat.add(Chat("Este easdasdasdasdasd","este easdasdasdasdasdasdasdasd"))
-        listachat.add(Chat("Este easdasdasdasdasd","este easdasdasdasdasdasdasdasd"))
-        listachat.add(Chat("Este easdasdasdasdasd","este easdasdasdasdasdasdasdasd"))
-        listachat.add(Chat("Este easdasdasdasdasd","este easdasdasdasdasdasdasdasd"))
-        listachat.add(Chat("Este easdasdasdasdasd","este easdasdasdasdasdasdasdasd"))
-        listachat.add(Chat("Este easdasdasdasdasd","este easdasdasdasdasdasdasdasd"))
-        listachat.add(Chat("Este easdasdasdasdasd","este easdasdasdasdasdasdasdasd"))
+        listachat.add(Chat("I've added some instructions. ","Here's a link to the post, which is on Edmodo, but I've made it public so you should be able to see it:"))
+        listachat.add(Chat("I've got a copy of the original CD, and would like to see the British Council put the whole thing on the internet. ","There were 32 tracks originally and I think it would be a useful pronunciation resource."))
+        listachat.add(Chat("On http://www.speakenglish.co.uk/phrases/travelling_by_air you have to click on the sentences to hear them, but at ","http://www.languageguide.org/english/vocabulary/travel/ you only have to move your mouse over the pictures "))
+        listachat.add(Chat("and here http://www.fonetiks.org/engsou6.html moving your mouse over the phrases plays the audio. I'd like the last style, but would still be happy with the first!","Great resources! Thanks a lot. "))
+        listachat.add(Chat("Would like to share an idea on using the dialogues ","in the classroom but the link doesn't seem to appear on the page. "))
+        listachat.add(Chat("Thank you so much you saved my life.","Now I have resources for Teaching Listening. "))
+        listachat.add(Chat("These audio files are copyright British Council.","You may download the files for your own personal or classroom use only. They may not be copied, "))
+        listachat.add(Chat("distributed or made available ","to other websites. "))
+        listachat.add(Chat("For any questions relating to the use of these programmes please email: teachingenglish@britishcouncil.org","file sizes for audio may be very large and can take a time to download depending on your connection speed. "))
+        listachat.add(Chat("I've added some instructions. ","Here's a link to the post, which is on Edmodo, but I've made it public so you should be able to see it:"))
+        listachat.add(Chat("I've got a copy of the original CD, and would like to see the British Council put the whole thing on the internet. ","There were 32 tracks originally and I think it would be a useful pronunciation resource."))
+        listachat.add(Chat("On http://www.speakenglish.co.uk/phrases/travelling_by_air you have to click on the sentences to hear them, but at ","http://www.languageguide.org/english/vocabulary/travel/ you only have to move your mouse over the pictures "))
+        listachat.add(Chat("and here http://www.fonetiks.org/engsou6.html moving your mouse over the phrases plays the audio. I'd like the last style, but would still be happy with the first!","Great resources! Thanks a lot. "))
+        listachat.add(Chat("Would like to share an idea on using the dialogues ","in the classroom but the link doesn't seem to appear on the page. "))
+        listachat.add(Chat("Thank you so much you saved my life.","Now I have resources for Teaching Listening. "))
+        listachat.add(Chat("These audio files are copyright British Council.","You may download the files for your own personal or classroom use only. They may not be copied, "))
+        listachat.add(Chat("distributed or made available ","to other websites. "))
+        listachat.add(Chat("For any questions relating to the use of these programmes please email: teachingenglish@britishcouncil.org","file sizes for audio may be very large and can take a time to download depending on your connection speed. "))
 
 
         iniciarRecylerView(listachat,this,rv_chats)
